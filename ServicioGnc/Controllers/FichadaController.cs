e954714f4a5443c8d8a791fec1c7b91ec19c2ec7
christianzilman <christianzilman@gmail.com>
2014-11-16 17:28:48 -0300

    Funcionalidad Fichada Se agrega, Se agrega un archivo datos.csv es el formato de cargo de datos de la fichada que se exporta por el dispositivo control

--- ServicioGnc/Controllers/FichadaController.cs
+++ ServicioGnc/Controllers/FichadaController.cs
@@ -7,6 +7,8 @@ using System.Web;
 using System.Web.Mvc;
 using ServicioGnc.DAL;
 using ServicioGnc.Models;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace ServicioGnc.Controllers
 {
@@ -19,9 +21,107 @@ namespace ServicioGnc.Controllers
 
         public ActionResult Index()
         {
+            //var fichadas = unitOfWork.FichadaRepository.Get();
+            //return View(fichadas.ToList());
+            return View();
+        }
+
+        public ActionResult Consulta()
+        {
             var fichadas = unitOfWork.FichadaRepository.Get();
             return View(fichadas.ToList());
+            return View();
         }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            if (Request.Files["file"].ContentLength > 0)
+            {
+                string fileExtension =
+                                     System.IO.Path.GetExtension(Request.Files["file"].FileName);
+
+                // work out where we should split on comma, but not in a sentence
+                Regex r = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+                StreamReader sr =  new StreamReader(Request.Files["file"].InputStream);
+
+                string line = string.Empty;
+                string[] strArray;
+                //Read the first line and split the string at , with our regular expression in to an array
+                line = sr.ReadLine();
+                strArray = r.Split(line);
+
+                
+                //Read each line in the CVS file until it’s empty
+                while ((line = sr.ReadLine()) != null)
+                {
+                    strArray = line.Split(',');
+                    Fichada fichada = new Fichada();
+
+                    int DiaIngreso = 0;
+                    int MesIngreso = 0;
+                    int AñoIngreso = 0;
+                    int HoraIngreso = 0;
+                    int MinutoIngreso = 0;
+
+                    int DiaEgreso = 0;
+                    int MesEgreso = 0;
+                    int AñoEgreso = 0;
+                    int HoraEgreso = 0;
+                    int MinutoEgreso = 0;
+
+                    for (int i = 0; i < strArray.Count();i++ )
+                    {
+                        if(i==0){
+                            String [] fechaI = strArray[i].Split('/');
+                            DiaIngreso = Convert.ToInt32(fechaI[0]);
+                            MesIngreso = Convert.ToInt32(fechaI[1]);
+                            AñoIngreso = Convert.ToInt32(fechaI[2]);
+                        }
+                        if(i==1){
+                            String[] horaI = strArray[i].Split(':');
+                            HoraIngreso = Convert.ToInt32(horaI[0]);
+                            MinutoIngreso = Convert.ToInt32(horaI[1]);
+                        }
+
+                        if (i == 2)
+                        {
+                            String[] fechaE = strArray[i].Split('/');
+                            DiaEgreso = Convert.ToInt32(fechaE[0]);
+                            MesEgreso = Convert.ToInt32(fechaE[1]);
+                            AñoEgreso = Convert.ToInt32(fechaE[2]);
+                        }
+                        if (i == 3)
+                        {
+                            String[] horaE = strArray[i].Split(':');
+                            HoraEgreso = Convert.ToInt32(horaE[0]);
+                            MinutoEgreso = Convert.ToInt32(horaE[1]);
+                        }
+                        if (i == 4)
+                        {
+                            fichada.PersonaId = Convert.ToInt32(strArray[i]);
+                        }
+
+                        if (i == 5)
+                        {
+                            fichada.Observacion = strArray[i];
+                        }
+                    }
+
+                    DateTime fechaIngresoCompleta = new DateTime(AñoIngreso, MesIngreso, DiaIngreso, HoraIngreso, MinutoIngreso, 0);
+                    DateTime fechaEgresoCompleta = new DateTime(AñoEgreso, MesEgreso, DiaEgreso, HoraEgreso, MinutoEgreso, 0);
+
+                    fichada.FechaIngreso = fechaIngresoCompleta;
+                    fichada.FechaEgreso = fechaEgresoCompleta;
+                    unitOfWork.FichadaRepository.Add(fichada);
+                    unitOfWork.Save();
+                    //Console.WriteLine(line);
+                }
+                //Tidy Streameader up
+                sr.Dispose();
+            }
+            return View("CargaExitosa");
+        }   
 
         //
         // GET: /Fichada/Details/5
