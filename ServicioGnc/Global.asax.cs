4b0ee96a80c7050a6f8453a25e1b4dae9a6676aa
christianzilman <christianzilman@gmail.com>
2014-12-09 20:30:30 -0300

    WebSecurity.InitializeDatabaseConnection

--- ServicioGnc/Global.asax.cs
+++ ServicioGnc/Global.asax.cs
@@ -1,21 +1,31 @@
-﻿using System;
+﻿using ServicioGnc.App_Start;
+using ServicioGnc.Filters;
+using ServicioGnc.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebMatrix.WebData;
+
 
 namespace ServicioGnc
 {
     // Nota: para obtener instrucciones sobre cómo habilitar el modo clásico de IIS6 o IIS7, 
     // visite http://go.microsoft.com/?LinkId=9394801
-
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+
+            try {
+                WebSecurity.InitializeDatabaseConnection("DefaultConnection",
+                "UserProfile", "UserId", "UserName", autoCreateTables: true);
+            }catch{}
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
@@ -23,6 +33,8 @@ namespace ServicioGnc
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+            
+         
         }
     }
 }
